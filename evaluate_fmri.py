08c2f8f28127513c1897488f16fb2b75cc064d19
benjaminht16 <133536963+benjaminht16@users.noreply.github.com>
2023-05-15 19:12:00 -0600

    Create evaluate_fmri.py

---
+++ evaluate_fmri.py
@@ -0,0 +1,79 @@
+import numpy as np  
+import pandas as pd  
+from nilearn import image, masking  
+from sklearn.model_selection import train_test_split, cross_val_score  
+from sklearn.linear_model import LogisticRegression  
+from sklearn.metrics import accuracy_score  
+ def preprocess_fmri(fmri_file: str, events_file: str, mask_file: str) -> np.ndarray:
+    """  
+    Preprocesses fMRI data, including concatenation of runs, confound removal, and masking.  
+    Args:  
+        fmri_file (str): Path to fMRI data file.  
+        events_file (str): Path to events file.  
+        mask_file (str): Path to mask file.  
+    Returns:  
+        preprocessed_data (numpy.ndarray): Preprocessed fMRI data.  
+    """  
+    # Load fMRI data  
+    fmri_img = image.load_img(fmri_file)  
+    # Load events  
+    events = pd.read_csv(events_file, delimiter='\t')  
+    # Concatenate runs  
+    fmri_data = image.concat_imgs(fmri_img)  
+    # Regress out confounds  
+    confounds = pd.read_csv(fmri_file.split('.')[0] + '_confounds.tsv', delimiter='\t')  
+    confound_cols = [col for col in confounds.columns if 'std' in col or 'motion' in col]  
+    confounds = confounds[confound_cols]  
+    fmri_data = masking.apply_mask(fmri_data, mask_file)  
+    reg = LogisticRegression().fit(confounds, fmri_data)  
+    fmri_data = fmri_data - reg.predict(confounds)  
+    # Apply mask  
+    mask_img = image.load_img(mask_file)  
+    fmri_data = masking.apply_mask(fmri_data, mask_img)  
+    return fmri_data  
+ def train_fmri_model(fmri_data: np.ndarray, labels: np.ndarray) -> LogisticRegression:  
+    """  
+    Trains a machine learning model on fMRI data.  
+    Args:  
+        fmri_data (numpy.ndarray): Preprocessed fMRI data.  
+        labels (numpy.ndarray): Corresponding labels for each time point.  
+    Returns:  
+        model (sklearn.linear_model.LogisticRegression): Trained machine learning model.  
+    """  
+    # Split data into training and testing sets  
+    X_train, X_test, y_train, y_test = train_test_split(fmri_data, labels, test_size=0.2, random_state=42)  
+    # Train model  
+    model = LogisticRegression().fit(X_train, y_train)  
+    return model  
+ def evaluate_fmri_model(model: LogisticRegression, fmri_data: np.ndarray, labels: np.ndarray) -> float:  
+    """  
+    Evaluates the performance of a machine learning model on fMRI data.  
+    Args:  
+        model (sklearn.linear_model.LogisticRegression): Trained machine learning model.  
+        fmri_data (numpy.ndarray): Preprocessed fMRI data.  
+        labels (numpy.ndarray): Corresponding labels for each time point.  
+    Returns:  
+        accuracy (float): Classification accuracy of the model.  
+    """  
+    # Predict labels  
+    y_pred = model.predict(fmri_data)  
+    # Calculate accuracy  
+    accuracy = accuracy_score(labels, y_pred)  
+    return accuracy  
+ def cross_validate_fmri_model(fmri_data: np.ndarray, labels: np.ndarray, cv: int = 5) -> float:  
+    """  
+    Performs cross-validation on a given machine learning model and fMRI data.  
+    Args:  
+        fmri_data (numpy.ndarray): Preprocessed fMRI data.  
+        labels (numpy.ndarray): Corresponding labels for each time point.  
+        cv (int, optional): Number of cross-validation folds to use. Defaults to 5.  
+    Returns:  
+        cv_accuracy (float): Average classification accuracy across all cross-validation folds.  
+    """  
+    # Initialize model  
+    model = LogisticRegression()  
+    # Perform cross-validation  
+    cv_scores = cross_val_score(model, fmri_data, labels, cv=cv)  
+    # Calculate average accuracy  
+    cv_accuracy = np.mean(cv_scores)  
+    return cv_accuracy in code
